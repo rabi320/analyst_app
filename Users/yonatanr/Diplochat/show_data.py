fb0ef36f7f5f28e751ede32dae797ac4fa574753
rabi320 <ryonatan6@gmail.com>
2024-08-01 10:08:39 +0000

    New App

--- Users/yonatanr/Diplochat/show_data.py
+++ Users/yonatanr/Diplochat/show_data.py
@@ -6,21 +6,21 @@ import pyodbc
 
 
 # Upload to SQL Server
-conn = pyodbc.connect(driver='{ODBC Driver 17 for SQL Server}',
-       server='diplomat-analytics-server.database.windows.net',
-       database='NBO-DB',
-       uid='analyticsadmin', pwd='Analytics12345')
+# conn = pyodbc.connect(driver='{ODBC Driver 17 for SQL Server}',
+#        server='diplomat-analytics-server.database.windows.net',
+#        database='NBO-DB',
+#        uid='analyticsadmin', pwd='Analytics12345')
 
 
-query = 'SELECT * FROM [dbo].[DW_DIM_STORENEXT_BY_INDUSTRIES_ITEMS]'
+# query = 'SELECT * FROM [dbo].[DW_DIM_STORENEXT_BY_INDUSTRIES_ITEMS]'
 
 
-# Execute the query and read the results into a DataFrame  
-df = pd.read_sql_query(query, conn)  
+# # Execute the query and read the results into a DataFrame  
+# df = pd.read_sql_query(query, conn)  
 
-conn.close()
+# conn.close()
 
-df.head()
+# df.head()
 
 
 
@@ -28,6 +28,6 @@ st.title("SQL Data Viewer")
     
     
 st.subheader("Table Head")  
-st.write(df.head())  
+# st.write(df.head())  
   
 
