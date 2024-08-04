1fc54fc4dd0470d751ce00621c351c577bfdb5b5
rabi320 <ryonatan6@gmail.com>
2024-08-04 18:09:21 +0000

    New Viz

--- Users/yonatanr/Diplochat/pages/visualizations.py
+++ Users/yonatanr/Diplochat/pages/visualizations.py
@@ -9,10 +9,12 @@ def load_data():
     conn = pyodbc.connect(driver='{ODBC Driver 17 for SQL Server}',
                           server='diplomat-analytics-server.database.windows.net',
                           database='NBO-DB',
-                          uid='analyticsadmin', pwd='Analytics12345')
+                          uid='analyticsadmin', 
+                          pwd='Analytics12345')
 
     query = 'SELECT * FROM [dbo].[DW_DIM_STORENEXT_BY_INDUSTRIES_ITEMS]'
     df = pd.read_sql_query(query, conn)
+    conn.close()  # Close the connection after reading the data
     return df
 
 def run():
@@ -22,11 +24,11 @@ def run():
     df = load_data()
 
     # Select a visualization type
-    viz_type = st.selectbox("Choose a visualization type", ["Category", "Class"])
+    viz_type = st.selectbox("Choose a visualization type", ["Bar Chart by Category", "Bar Chart by Class", "Line Chart of Sales Over Time"])
 
-    if viz_type == "Bar Chart":
-        # Create a bar chart
-        st.subheader("Bar Chart of Item Counts by Industry")
+    if viz_type == "Bar Chart by Category":
+        # Create a bar chart of counts by Category
+        st.subheader("Bar Chart of Item Counts by Category")
         industry_counts = df['Category_Name'].value_counts()  # Replace with actual column name
         fig, ax = plt.subplots()
         sns.barplot(x=industry_counts.index, y=industry_counts.values, ax=ax)
@@ -34,12 +36,12 @@ def run():
         plt.xticks(rotation=45)
         st.pyplot(fig)
 
-    elif viz_type == "Line Chart":
-        # Create a bar chart
+    elif viz_type == "Bar Chart by Class":
+        # Create a bar chart of counts by Class
         st.subheader("Bar Chart of Item Counts by Class")
-        industry_counts = df['Class_Name'].value_counts()  # Replace with actual column name
+        class_counts = df['Class_Name'].value_counts()  # Replace with actual column name
         fig, ax = plt.subplots()
-        sns.barplot(x=industry_counts.index, y=industry_counts.values, ax=ax)
+        sns.barplot(x=class_counts.index, y=class_counts.values, ax=ax)
         ax.set_title("Count of Items by Class")
         plt.xticks(rotation=45)
         st.pyplot(fig)
