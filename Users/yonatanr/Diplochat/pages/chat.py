319d3f93e6845e2ac2ed2235d027f7b90c4bee2a
rabi320 <ryonatan6@gmail.com>
2024-08-04 19:06:01 +0000

    Chat Final

--- Users/yonatanr/Diplochat/pages/chat.py
+++ Users/yonatanr/Diplochat/pages/chat.py
@@ -3,81 +3,83 @@
 from openai import OpenAI
 import streamlit as st
 
-sys_message = """
-You Are a helpful AI Data Analyst Assistant for Diplomat Distributors LTD - Youre offical name is: Diplochat.
 
-if asked on your abilities explain that you are under development progress and new abilities will be shown in the future.
+def run():
+    sys_message = """
+    You Are a helpful AI Data Analyst Assistant for Diplomat Distributors LTD - Youre offical name is: Diplochat.
 
-About your model version:
+    if asked on your abilities explain that you are under development progress and new abilities will be shown in the future.
 
-Your gpt versions name is GPT-4o mini released in july 18th 2024, and here is some info on it:
+    About your model version:
 
-GPT-4o ("o" for "omni") and GPT-4o mini are natively multimodal models designed to handle a combination of text, audio, and video inputs, and can generate outputs in text, audio, and image formats. GPT-4o mini is the lightweight version of GPT-4o.
+    Your gpt versions name is GPT-4o mini released in july 18th 2024, and here is some info on it:
 
-Background
-Before GPT-4o, users could interact with ChatGPT using Voice Mode, which operated with three separate models. GPT-4o integrates these capabilities into a single model that's trained across text, vision, and audio. This unified approach ensures that all inputs — whether text, visual, or auditory — are processed cohesively by the same neural network.
+    GPT-4o ("o" for "omni") and GPT-4o mini are natively multimodal models designed to handle a combination of text, audio, and video inputs, and can generate outputs in text, audio, and image formats. GPT-4o mini is the lightweight version of GPT-4o.
 
-GPT-4o mini is the next iteration of this omni model family, available in a smaller and cheaper version. This model offers higher accuracy than GPT-3.5 Turbo while being just as fast and supporting multimodal inputs and outputs.
-"""
+    Background
+    Before GPT-4o, users could interact with ChatGPT using Voice Mode, which operated with three separate models. GPT-4o integrates these capabilities into a single model that's trained across text, vision, and audio. This unified approach ensures that all inputs — whether text, visual, or auditory — are processed cohesively by the same neural network.
 
-st.title("Diplomat's AI Assistant 🤖")
+    GPT-4o mini is the next iteration of this omni model family, available in a smaller and cheaper version. This model offers higher accuracy than GPT-3.5 Turbo while being just as fast and supporting multimodal inputs and outputs.
+    """
 
-# Disclaimer
-disclaimer = """
-**Disclaimer:**
+    st.title("Diplomat's AI Assistant 🤖")
 
-The information provided by this AI assistant is generated based on available data and patterns, and it may not always be accurate or up-to-date. 
-Users are advised to independently verify any critical information and exercise their judgment when relying on the assistant's responses. 
-The developers and creators of this AI assistant are not liable for any inaccuracies, errors, or consequences resulting from the use of the provided information.
-"""
+    # Disclaimer
+    disclaimer = """
+    **Disclaimer:**
 
-# Display the disclaimer
-st.markdown(disclaimer)
+    The information provided by this AI assistant is generated based on available data and patterns, and it may not always be accurate or up-to-date. 
+    Users are advised to independently verify any critical information and exercise their judgment when relying on the assistant's responses. 
+    The developers and creators of this AI assistant are not liable for any inaccuracies, errors, or consequences resulting from the use of the provided information.
+    """
 
-uploaded_file = st.file_uploader("Choose an Avatar for yourself: 📷🧑", type=["jpg", "jpeg", "png"])
-if uploaded_file is not None:
-    image= uploaded_file.read()
-    user_avatar = image
-else:
-    user_avatar = '🧑'
+    # Display the disclaimer
+    st.markdown(disclaimer)
+
+    uploaded_file = st.file_uploader("Choose an Avatar for yourself: 📷🧑", type=["jpg", "jpeg", "png"])
+    if uploaded_file is not None:
+        image= uploaded_file.read()
+        user_avatar = image
+    else:
+        user_avatar = '🧑'
 
 
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    with st.sidebar:
+        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 
-client = OpenAI(api_key=openai_api_key)
+    client = OpenAI(api_key=openai_api_key)
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4o-mini"
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-4o-mini"
 
-if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "system", "content": sys_message}]
+    if "messages" not in st.session_state:
+        st.session_state.messages = [{"role": "system", "content": sys_message}]
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    
-    if message["role"]=='assistant':
-        with st.chat_message(message["role"], avatar='🤖'):
-            st.markdown(message["content"])
-    elif message["role"]=='user':
-        with st.chat_message(message["role"], avatar=user_avatar):
-            st.markdown(message["content"])
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        
+        if message["role"]=='assistant':
+            with st.chat_message(message["role"], avatar='🤖'):
+                st.markdown(message["content"])
+        elif message["role"]=='user':
+            with st.chat_message(message["role"], avatar=user_avatar):
+                st.markdown(message["content"])
 
-if prompt := st.chat_input("Ask me anything"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user", avatar=user_avatar):
-        st.markdown(prompt)
+    if prompt := st.chat_input("Ask me anything"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user", avatar=user_avatar):
+            st.markdown(prompt)
 
-    with st.chat_message("assistant", avatar='🤖'):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            max_tokens=500,
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        with st.chat_message("assistant", avatar='🤖'):
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                max_tokens=500,
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
