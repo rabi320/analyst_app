999abbd1f3a4c0911af20ea6aa9c1038e9727151
rabi320 <ryonatan6@gmail.com>
2024-08-05 07:21:40 +0000

    No API Key

--- Users/yonatanr/Diplochat/pages/chat.py
+++ Users/yonatanr/Diplochat/pages/chat.py
@@ -46,8 +46,8 @@ def run():
 
 
 
-    with st.sidebar:
-        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    # with st.sidebar:
+    #     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 
     # client = OpenAI(api_key=openai_api_key)
     client = AzureOpenAI(
