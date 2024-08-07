a579362af97130518f63390c788476559394477f
rabi320 <ryonatan6@gmail.com>
2024-08-07 20:47:44 +0000

    smaller data

--- Users/yonatanr/Diplochat/pages/script_run.py
+++ Users/yonatanr/Diplochat/pages/script_run.py
@@ -16,18 +16,18 @@ def load_data():
         'DW_FACT_STORENEXT_BY_INDUSTRIES_SALES': """
             SELECT Day, Barcode, Format_Name, Sales_NIS, Sales_Units, Price_Per_Unit
             FROM [dbo].[DW_FACT_STORENEXT_BY_INDUSTRIES_SALES]
-            WHERE Day BETWEEN '2024-03-01' AND '2024-05-31'
+            WHERE Day BETWEEN '2024-05-01' AND '2024-05-31'
         """,
         'DW_DIM_STORENEXT_BY_INDUSTRIES_ITEMS': """
             SELECT Barcode, Item_Name, Category_Name, Sub_Category_Name, Brand_Name, Sub_Brand_Name, Supplier_Name
             FROM [dbo].[DW_DIM_STORENEXT_BY_INDUSTRIES_ITEMS]
             WHERE Category_Name = N'חטיפים'
         """,
-        'DW_CHP': """
-            SELECT ITEM_DESCRIPION, BARCODE, CHAIN_CODE, STORE_CODE, CHAIN, STORE, ADDRESS, CITY, SELLOUT_DESCRIPTION, STORENEXT_CATEGORY, SUPPLIER, FILE_DATE, PRICE, SELLOUT_PRICE, SALE_ID
-            FROM [dbo].[DW_CHP]
-            WHERE STORENEXT_CATEGORY = N'חטיפים' AND FILE_DATE BETWEEN '2024-05-30' AND '2024-05-31'
-        """
+        # 'DW_CHP': """
+        #     SELECT ITEM_DESCRIPION, BARCODE, CHAIN_CODE, STORE_CODE, CHAIN, STORE, ADDRESS, CITY, SELLOUT_DESCRIPTION, STORENEXT_CATEGORY, SUPPLIER, FILE_DATE, PRICE, SELLOUT_PRICE, SALE_ID
+        #     FROM [dbo].[DW_CHP]
+        #     WHERE STORENEXT_CATEGORY = N'חטיפים' AND FILE_DATE BETWEEN '2024-05-30' AND '2024-05-31'
+        # """
     }
 
     dataframes = {}  
@@ -63,7 +63,7 @@ def run():
                           value='answer = "Hello, this is the answer!"')  
   
     if st.button("Run Code"):  
-        local_context = {'chp': chp,'stnx_sales':stnx_sales,'stnx_items':stnx_items,'pd':pd}  
+        local_context = {'stnx_sales':stnx_sales,'stnx_items':stnx_items,'pd':pd}  
   
         try:  
             exec(script.strip(), {}, local_context)  
