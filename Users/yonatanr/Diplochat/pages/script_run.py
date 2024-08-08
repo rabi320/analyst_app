db2d3fe40084834dddc04544c82716881ebda5e9
rabi320 <ryonatan6@gmail.com>
2024-08-08 10:25:19 +0000

    chp tel aviv only

--- Users/yonatanr/Diplochat/pages/script_run.py
+++ Users/yonatanr/Diplochat/pages/script_run.py
@@ -56,7 +56,7 @@ def run():
     # Assigning dataframes to variables
     stnx_sales = dataframes['DW_FACT_STORENEXT_BY_INDUSTRIES_SALES']
     stnx_items = dataframes['DW_DIM_STORENEXT_BY_INDUSTRIES_ITEMS']
-    # chp = dataframes['DW_CHP']
+    chp = dataframes['DW_CHP']
 
     
     script = st.text_area("Enter your Python code:", height=200,  
