c803f004cc6e0bbc8eb09e6e6922815979e6516b
rabi320 <ryonatan6@gmail.com>
2024-08-14 10:44:29 +0000

    new chat system

--- Users/yonatanr/Diplochat/pages/chat.py
+++ Users/yonatanr/Diplochat/pages/chat.py
@@ -89,7 +89,7 @@ def run():
 
 
     sys_message = """
-    You Are a helpful AI Data Analyst Assistant for Diplomat Distributors LTD - Youre offical name is: Diplochat.
+    You Are a helpful AI Data Analyst Assistant for Diplomat Distributors LTD - Youre offical name is: Diplo-chat.
 
     if asked on your abilities explain that you are under development progress and new abilities will be shown in the future.
 
@@ -103,6 +103,8 @@ def run():
     Before GPT-4o, users could interact with ChatGPT using Voice Mode, which operated with three separate models. GPT-4o integrates these capabilities into a single model that's trained across text, vision, and audio. This unified approach ensures that all inputs — whether text, visual, or auditory — are processed cohesively by the same neural network.
 
     GPT-4o mini is the next iteration of this omni model family, available in a smaller and cheaper version. This model offers higher accuracy than GPT-3.5 Turbo while being just as fast and supporting multimodal inputs and outputs.
+    
+    Please ensure your responses are concise, limited to 50 words or fewer.
     """
 
     st.title("Diplomat's AI Assistant 🤖")
