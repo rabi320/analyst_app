c196e48966dd98776d27c0ff9b718cdd1e230d64
rabi320 <ryonatan6@gmail.com>
2024-08-21 11:59:46 +0000

    callback

--- Users/yonatanr/Diplochat/pages/analyst_chat.py
+++ Users/yonatanr/Diplochat/pages/analyst_chat.py
@@ -555,13 +555,22 @@ def run():
             st.dataframe(log_df)
         
         with st.expander("Rate me!"): 
-            sentiment_mapping = ["one", "two", "three", "four", "five"]
-            feedback = st.feedback("stars") 
-            if feedback is not None:
-                st.session_state.user_feedback = feedback
+            # sentiment_mapping = ["one", "two", "three", "four", "five"]
+            # feedback = st.feedback("stars")
 
-                # st.markdown(f"You selected {sentiment_mapping[st.session_state.user_feedback]} star(s).")
-                log_df.loc[len(log_df)-1,'Final_Answer'] = st.session_state.user_feedback               
+            def update_feedback(feedback):  
+                if feedback is not None:  
+                    st.session_state.user_feedback = feedback  
+                    log_df.at[len(log_df)-1, 'Final_Answer'] = st.session_state.user_feedback  
+            
+            feedback = st.feedback("stars", key="user_feedback", on_change=update_feedback)
+
+                        # if feedback is not None:
+                
+            #     st.session_state.user_feedback = feedback
+
+            #     # st.markdown(f"You selected {sentiment_mapping[st.session_state.user_feedback]} star(s).")
+            #     log_df.loc[len(log_df)-1,'Final_Answer'] = st.session_state.user_feedback               
 
 
 
