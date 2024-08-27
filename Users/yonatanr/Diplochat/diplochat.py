963c9df748c898900b5451d6e022095fa61855dc
rabi320 <ryonatan6@gmail.com>
2024-08-27 13:44:39 +0000

    reset password second attempt

--- Users/yonatanr/Diplochat/diplochat.py
+++ Users/yonatanr/Diplochat/diplochat.py
@@ -29,9 +29,10 @@ if st.session_state['authentication_status']:
     # reset password
     try:
         if authenticator.reset_password(st.session_state['username']):
-            st.success('Password modified successfully')
+            
             with open('../config.yaml', 'w') as file:
                 yaml.dump(config, file, default_flow_style=False)
+            st.success('Password modified successfully')
     except Exception as e:
         st.error(e)
 
