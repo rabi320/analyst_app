58f6d982214c87ff3778c9e75084b2156cedd496
rabi320 <ryonatan6@gmail.com>
2024-08-27 13:43:28 +0000

    reset password attempt

--- Users/yonatanr/Diplochat/diplochat.py
+++ Users/yonatanr/Diplochat/diplochat.py
@@ -25,6 +25,16 @@ authentication_status = authenticator.login()
 
 # Adjusted authentication status handling  
 if st.session_state['authentication_status']:  
+    
+    # reset password
+    try:
+        if authenticator.reset_password(st.session_state['username']):
+            st.success('Password modified successfully')
+            with open('../config.yaml', 'w') as file:
+                yaml.dump(config, file, default_flow_style=False)
+    except Exception as e:
+        st.error(e)
+
     authenticator.logout()  # Add logout functionality  
     st.write(f'Welcome *{st.session_state["name"]}*')  # Display welcome message  
     st.title('Diplomat AI')  # Replace with your actual content  
