b6610aa51dfb0112aed8701d930780a825040e60
rabi320 <ryonatan6@gmail.com>
2024-08-29 09:42:20 +0000

    new style text

--- Users/yonatanr/Diplochat/pages/home.py
+++ Users/yonatanr/Diplochat/pages/home.py
@@ -2,4 +2,16 @@ import streamlit as st
 
 def run():
     st.header("Welcome to the Analytics Hub")
-    st.markdown('<p style="color: blue; font-size: 20px;"><strong>Committed to Your Success</strong></p>', unsafe_allow_html=True)
+    # st.markdown('<p style="color: blue; font-size: 20px;"><strong>Committed to Your Success</strong></p>', unsafe_allow_html=True)
+    # Adding stylized text with a cool gradient and shadow effect
+    style_txt = """
+    <h1 style="text-align: center; font-size: 40px; 
+        color: cyan; 
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); 
+        background: linear-gradient(90deg, #00f, #0ff);
+        -webkit-background-clip: text; 
+        -webkit-text-fill-color: transparent;">
+        <strong>Committed to Your Success</strong>
+        </h1>
+    """
+    st.markdown(style_txt, unsafe_allow_html=True)
