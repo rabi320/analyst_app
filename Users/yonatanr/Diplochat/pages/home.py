eec1a17e146af3502dd91bf4d5781b76b7903589
rabi320 <ryonatan6@gmail.com>
2024-08-29 10:54:42 +0000

    New example

--- Users/yonatanr/Diplochat/pages/home.py
+++ Users/yonatanr/Diplochat/pages/home.py
@@ -5,7 +5,7 @@ def run():
     # st.markdown('<p style="color: blue; font-size: 20px;"><strong>Committed to Your Success</strong></p>', unsafe_allow_html=True)
     # Adding stylized text with a cool gradient and shadow effect
     style_txt = """
-    <h1 style="text-align: center; font-size: 40px; 
+    <h1 style="text-align: center; font-size: 20px; 
         color: cyan; 
         text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); 
         background: linear-gradient(90deg, #0ff 70%, #e0f7fa);
