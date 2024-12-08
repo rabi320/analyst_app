ead6fa996dfb0068dc4b6094c7fbadf2c82cddca
rabi320 <ryonatan6@gmail.com>
2024-12-08 15:06:34 +0000

    No login form

--- Users/yonatanr/Diplochat/Diplochat.py
+++ Users/yonatanr/Diplochat/Diplochat.py
@@ -48,20 +48,20 @@ if 'cache_cleared' not in st.session_state:
 # Login widget  
 authentication_status = authenticator.login()  
 
-# Button to show the form
-if st.button("Sign Up"):
-    # Create a form for user input
-    with st.form(key='signup_form'):
-        email = st.text_input("Email Address")
-        full_name = st.text_input("Full Name")
+# # Button to show the form
+# if st.button("Sign Up"):
+#     # Create a form for user input
+#     with st.form(key='signup_form'):
+#         email = st.text_input("Email Address")
+#         full_name = st.text_input("Full Name")
 
-        submit_button = st.form_submit_button(label='Sign Up')
+#         submit_button = st.form_submit_button(label='Sign Up')
 
-        if submit_button:
-            # Display the entered information
-            st.success("You have signed up successfully!")
-            st.write("Email Address:", email)
-            st.write("Full Name:", full_name)
+#         if submit_button:
+#             # Display the entered information
+#             st.success("You have signed up successfully!")
+#             st.write("Email Address:", email)
+#             st.write("Full Name:", full_name)
 
 
 
