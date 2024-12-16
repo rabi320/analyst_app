d3289d8a16e07a42f2e4a0733b9547ba3fd74a86
rabidip1 <yonatanr@diplomat-global.com>
2024-12-16 11:28:30 +0000

    Diplochat Dev test

--- Users/yonatanr/Diplochat/diplochat_dev.py
+++ Users/yonatanr/Diplochat/diplochat_dev.py
@@ -17,6 +17,7 @@ with st.form(key='signup_form'):
         st.success("You have signed up successfully!")
         st.write("Email Address:", email)
         st.write("Full Name:", full_name)
+        
 
         # It's not a good practice to display passwords, but for the sake of this example:
         st.write("Password:", password)
