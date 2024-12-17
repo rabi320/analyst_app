7f9f2e29dc76682a7069c6569a94d9456900b320
rabi320 <ryonatan6@gmail.com>
2024-12-17 13:56:38 +0000

    fix signup tabs spaces

--- Users/yonatanr/Diplochat/Diplochat.py
+++ Users/yonatanr/Diplochat/Diplochat.py
@@ -206,12 +206,15 @@ if st.session_state['authentication_status']:
         cursor.close()
         st.toast(f"✔️ User {full_name} signed up successfully with email: {email}!")
 
+
+
     def user_signup(full_name,email):
-    	if email in user_df.eamil.values:
+        if email in user_df.eamil.values:
             st.toast(f"❌ User {full_name} already in the system with mail: {email}!")
         elif '@' not in email or '.co' not in email:
             st.toast(f"❌ {email} not a valid email!")
         else:
+
             conn = pyodbc.connect(driver='{ODBC Driver 17 for SQL Server}',  
                     server='diplomat-analytics-server.database.windows.net',  
                     database='Diplochat-DB',  
@@ -220,21 +223,26 @@ if st.session_state['authentication_status']:
             insert_query = """  
             INSERT INTO DW_DIM_USERS (username, email, failed_login_attempts, logged_in, name, password)  
             VALUES (?, ?, ?, ?, ?, ?)  
-            """	
+            """  
+
             # username
             username = email.split('@')[0]
             password = email.split('@')[0]+''.join(str(i+1) for i in range(len(email.split('@')[0])))+'!'
             password = password.capitalize()
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()) 
-        
+            
             log_session = [username,email,0,0,full_name,hashed_password.decode('utf-8')]
 
             cursor = conn.cursor()
             cursor.execute(insert_query, log_session)
-        
+            
+
+
             conn.commit()  
             cursor.close()
             st.toast(f"✔️ User {full_name} signed up successfully with email: {email}!")
+
+
 
     # Check if the current user is an admin
     if st.session_state.get("name") in admin_list:
